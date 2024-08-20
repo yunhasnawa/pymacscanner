7efb146a60106b44e95850452eb1d5bc53018e18
Yoppy Yunhasnawa <yunhasnawa@ymail.com>
2024-08-20 13:24:13 +0700

    Update installer script and service

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,5 @@
+sudo cp -r ../../pymacscanner /usr/local/pymacscanner
 sudo cp pymacscanner.service /etc/systemd/system/pymacscanner.service
 sudo systemctl daemon-reload
-sudo systemctl enable my_python_script.service
-sudo systemctl start my_python_script.service
+sudo systemctl enable pymacscanner.service
+sudo systemctl start pymacscanner.service
